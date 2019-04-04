cf85c97fd4f8bc49973309a62f4c24af7d39b409
Sebastian <sebgertz@gmail.com>
2019-04-04 12:46:14 -0400

    add approval functions

--- escrow.sol
+++ escrow.sol
@@ -2,13 +2,22 @@ pragma solidity 0.5.7;
 
 contract Escrow {
     address public sender;
-    address public receiver;
+    address payable public receiver;
     address public constant approver = 0xabeD470Cf6aebce5530A363E0F5f8334A9A252f4;
 
 
-    function deposit(address _receiver) external payable {
+    function deposit(address payable _receiver) external payable {
         require(msg.value > 0);
         sender = msg.sender;
         receiver = _receiver;
+    }
+
+    function viewApprover() external pure returns(address) {
+        return(approver);
+    }
+
+    function approve() external {
+        require(msg.sender == approver);
+        receiver.transfer(address(this).balance);
     }
 }
